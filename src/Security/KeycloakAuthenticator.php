9b418c54fcf9e067a4fef336a8a7a7e372b3b59b
Maxat <Maxat@DESKTOP-RUR87SH>
2023-02-10 12:36:30 +0100

    Implémentation d'une authentification pour l'accès au back office en utilisant Keycloak : Problème -> impossible d'accéder au formulaire de connexion en arrivant sur les pages /adminFormations, /adminCategories, ou /adminPlaylists. " Page not found " Correction de quelques erreurs de frappe et de use.

--- src/Security/KeycloakAuthenticator.php
+++ src/Security/KeycloakAuthenticator.php
@@ -2,6 +2,7 @@
 
 namespace App\Security;
 
+use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
 use KnpU\OAuth2ClientBundle\Security\Authenticator\OAuth2Authenticator;
@@ -11,7 +12,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Core\User\User;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
@@ -74,7 +74,7 @@ class KeycloakAuthenticator extends OAuth2Authenticator implements Authenticatio
                     }
                     //3) le user n'existe pas encore dans la BDD
                     $user = new User();
-                    $user->setKeycloakdId($keycloakUser->getId());
+                    $user->setKeycloakId($keycloakUser->getId());
                     $user->setEmail($keycloakUser->getEmail());
                     $user->setPassword("");
                     $user->setRoles(['ROLE_ADMIN']);
